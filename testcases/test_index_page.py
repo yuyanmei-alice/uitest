7ea6249c8f0a04d78c4ac72fb24fcb7d9d5b2387
yuyanmei-alice <72970084+yuyanmei-alice@users.noreply.github.com>
2021-04-15 14:39:40 +0800

    Update test_index_page.py

--- testcases/test_index_page.py
+++ testcases/test_index_page.py
@@ -1,1 +1,21 @@
 
+from pages.index_page import IndexPage
+
+
+class TestIndexPage:
+
+    def setup(self):
+        self.index_page = IndexPage()
+        self.index_page.start()
+
+    def test_search(self):
+        """
+        验证首页的搜索功能
+        :return:
+        """
+        self.index_page.search("算法")
+        assert self.index_page.ielement_is_exsit("id", 'loginsubmit')
+
+    def teardown(self):
+        self.index_page.close()
+
