3e97d0e7f8aaa594c9262fb8f13be14eb0ea15f6
yuyanmei-alice <72970084+yuyanmei-alice@users.noreply.github.com>
2021-04-15 14:30:04 +0800

    Update yaml_transfer.py

--- utils/yaml_transfer.py
+++ utils/yaml_transfer.py
@@ -1,1 +1,26 @@
 
+import chevron
+import yaml
+
+
+class YamlTransfer:
+    @staticmethod
+    def get_yaml_content(filepath, render_dict):
+        """
+        cheveron渲染yaml文件
+        :param render_dict:根据yaml文件中的参数化内容传入的参数值
+        mustache用 {{}}代表要进行替换的参数
+        :return:
+        """
+        with open(filepath, encoding='UTF-8') as f:
+            result = yaml.safe_load(chevron.render(f, render_dict))
+            return result
+
+    @staticmethod
+    def get_yaml():
+        """
+        举例：使用mustache技术进行渲染
+        :return:
+        """
+        result = chevron.render('Hello, {{ mustache }}!', {'mustache': '[\'1\',\'2\',\'3\']'})
+        return result
