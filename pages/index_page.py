a990d236f0a0acf16a3f8ef7813c7ff852bb981f
yuyanmei-alice <72970084+yuyanmei-alice@users.noreply.github.com>
2021-04-15 14:39:12 +0800

    Update index_page.py

--- pages/index_page.py
+++ pages/index_page.py
@@ -1,1 +1,21 @@
 
+from pages.base_page import BasePage
+
+from utils.yaml_transfer import YamlTransfer
+import os
+
+
+FILE_PATH = os.path.join(os.getcwd(), r'yamls\index_page.yaml')
+
+
+class IndexPage(BasePage):
+    def search(self, keywords):
+        """
+        :param keywords: 输入关键字查询
+        :return:
+        """
+        data = {"keywords": keywords}
+        yaml_content = YamlTransfer.get_yaml_content(FILE_PATH, data)
+        self.exec_steps(yaml_content, 'search')
+        from pages.login_page import LoginPage
+        return  LoginPage(self.driver)
