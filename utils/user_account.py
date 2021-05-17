f29ddf4c83659a0277e25237ba08cc0364bd16ff
yuyanmei-alice <72970084+yuyanmei-alice@users.noreply.github.com>
2021-05-17 16:24:59 +0800

    Update user_account.py

--- utils/user_account.py
+++ utils/user_account.py
@@ -1,1 +1,14 @@
 
+import yaml
+
+
+class UserAccount:
+    @staticmethod
+    def get_user_info_by_role(roleName):
+        """
+        :param roleName: 角色名
+        :return: 根据传入的角色名返回角色信息：用户名和密码
+        """
+        with open("../data/user_acount.yaml", encoding='UTF-8') as f:
+            result = yaml.safe_load(f)[roleName][0]
+            return result
